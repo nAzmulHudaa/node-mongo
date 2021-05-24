b8f964297b649916a0bd86b8ad51d98f8ed265da
Md Nazmul Huda Rimon <nhr.developer@gmail.com>
2021-05-24 13:47:23 +0600

    created my first api

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require("express");
 
 const app = express();
 
+const users = ["Asad","Moin","Sabed","Sabana","Suhana"];
 app.get('/',(req,res)=>{
     res.send("Hello I am working");
 })
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:'banana',quantity:1000,price:100000})
+})
+app.get('/users/:id',(req,res)=>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id,name});
+})
 
-
-app.listen(3000,()=>{
-    console.log("listening to port 3000")
+app.listen(4000,()=>{
+    console.log("listening to port 4000")
 })
